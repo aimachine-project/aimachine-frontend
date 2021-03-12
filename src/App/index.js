1964a34fe20c279d993faf08e07e112575709417
agakesik <aga.k396@gmail.com>
2021-03-12 15:07:21 +0100

    Add and setup Tailwind to project

--- src/App/index.js
+++ src/App/index.js
@@ -5,6 +5,16 @@ function App() {
   return (
     <div className="App">
       <p>hello world!</p>
+      <div className="flex flex-row-reverse">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+      <div className="flex flex-col">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
     </div>
   );
 }
