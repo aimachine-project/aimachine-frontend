99df83ded7b1179ce5bb746947fa78143a47b2e5
agakesik <aga.k396@gmail.com>
2021-03-12 18:59:12 +0100

    Create base styling for h1 and adjust home page view to have basic preview

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,12 +5,12 @@ function Home() {
     <div>
       <h1>home page!</h1>
       <p>this is where we&apos;ll have our games to choose</p>
-      <a href="/game">
-        <div>
-          <h2>game name</h2>
-          <p>currently playing: 50 people!</p>
-        </div>
-      </a>
+      <div className="m-40">
+        <a href="/game" className="border-2 p-2 m-6 bg-gray-400">
+          <span className="text-2xl">Game Name</span>
+        </a>
+        <span className="text-xs">currently playing: 50 people!</span>
+      </div>
     </div>
   );
 }
