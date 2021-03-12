60e00fcdd685141800cfb7aae53fd2c2bce4aef8
agakesik <aga.k396@gmail.com>
2021-03-12 19:02:49 +0100

    Fix navbar link description

--- src/Navbar/index.js
+++ src/Navbar/index.js
@@ -6,11 +6,11 @@ function Navbar() {
     <div className="p-2 bg-gray-300 flex flex-row justify-around lg:flex-col lg:justify-start">
       <a href="/" className="p-3">
         <BiHome className="text-5xl" aria-hidden="true" />
-        <p className="hidden lg:visible">Home</p>
+        <p className="invisible lg:visible">Home</p>
       </a>
       <a href="/" className="p-3">
         <BiUser className="text-5xl" aria-hidden="true" />
-        <p className="hidden lg:visible">Log In</p>
+        <p className="invisible lg:visible">Log In</p>
       </a>
     </div>
   );
