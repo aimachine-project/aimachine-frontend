02d6c96287d11370fd3f25d6285d2c2c273d652e
Aga Kesik <aga.k396@gmail.com>
2021-06-30 15:27:40 +0200

    Make request to registration API respond to status other than ok and fix minor errors

--- src/pages/Login/components/RegisterForm.js
+++ src/pages/Login/components/RegisterForm.js
@@ -6,21 +6,30 @@ function RegistrationForm() {
   const [doesPasswordMatch, setDoesPasswordMatch] = useState(true);
   // const [test, setTest] = useState("");
 
-  const handleSubmit = () => {
-    if (user.username === "" || user.password === "") {
-      alert("nie można!");
-      return;
-    }
+  const handleSubmit = (event) => {
+    event.preventDefault();
     fetch("http://localhost:8080/api/register", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(user),
-      // mode: "no-cors",
+      body: JSON.stringify({
+        username: user.username,
+        password: user.password,
+      }),
     })
-      // .then((response) => console.log(response.status))
-      .then((response) => response.json())
-      .then((json) => console.log(json));
+      .then((response) => {
+        console.log("test");
+        if (response.ok) {
+          response.json().then((json) => {
+            console.log(json);
+            window.location.reload();
+          });
+        } else {
+          console.log("response is not ok");
+        }
+      })
+      .catch((error) => console.log(error));
   };
+
   const onChange = (e) => setUser({ ...user, [e.target.name]: e.target.value });
   const confirmPassword = (e) => {
     const value = e.target.value;
