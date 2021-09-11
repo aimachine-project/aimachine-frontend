c78028b56185d698e1a4991620ffcff9e8b8e6cd
Sebastian Syska <s.syska@samsung.com>
2021-09-11 18:20:44 +0200

    change login path

--- src/utilities/URL.js
+++ src/utilities/URL.js
@@ -1,6 +1,6 @@
 const baseApi = "http://" + document.domain + ":8080";
 export const REGISTER_URL = baseApi + "/api/users/create";
-export const LOGIN_URL = baseApi + "/api/users/self";
+export const LOGIN_URL = baseApi + "/api/login";
 export const LOGOUT_URL = baseApi + "/api/logout";
 export const USER_INFO_URL = baseApi + "/api/users/self";
 
