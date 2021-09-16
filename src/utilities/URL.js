ce13a0317d90fff8246b9dbfc2992c382a71f362
Sebastian Syska <s.syska@samsung.com>
2021-09-16 17:12:13 +0200

    rm urls and revert SocketHelper

--- src/utilities/URL.js
+++ src/utilities/URL.js
@@ -3,8 +3,3 @@ export const REGISTER_URL = baseApi + "/api/users/create";
 export const LOGIN_URL = baseApi + "/api/login";
 export const LOGOUT_URL = baseApi + "/api/logout";
 export const USER_INFO_URL = baseApi + "/api/users/self";
-
-const protocol = location.protocol.replace("http", "ws");
-const baseWebSocket = protocol + "//" + document.domain + ":8080";
-export const TICTACTOE_URL = baseWebSocket + "/games/tictactoe";
-export const TICTACTOEEXP_URL = baseWebSocket + "/games/tictactoenfields";
