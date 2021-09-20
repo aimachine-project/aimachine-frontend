e5cccae0e1c24fa25d47099b0025a8d6ec3fb98e
agakesik <aga.k396@gmail.com>
2021-09-20 17:05:00 +0200

    Test using canvas elemenet

--- src/games/Soccer/components/SoccerBoard.js
+++ src/games/Soccer/components/SoccerBoard.js
@@ -1,6 +1,40 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 function SoccerBoard() {
-  return <div className="board">board</div>;
+  const canvasRef = useRef(null);
+
+  const boardStyle = { border: "1px solid #000000" };
+  useEffect(() => {
+    const canvasObj = canvasRef.current;
+    const ctx = canvasObj.getContext("2d");
+    ctx.beginPath();
+    ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+    ctx.stroke();
+  });
+  const handleCanvasClick = (event) => {
+    // on each click get current mouse location
+    const currentCoord = { x: event.clientX, y: event.clientY };
+    // add the newest mouse location to an array in state
+    console.log(currentCoord.x + " " + currentCoord.y);
+    const canvasObj = canvasRef.current;
+    const ctx = canvasObj.getContext("2d");
+    ctx.moveTo(0, 0);
+    ctx.lineTo(currentCoord.x, currentCoord.y);
+    ctx.stroke();
+    setCoordinates([...coordinates, currentCoord]);
+  };
+
+  return (
+    <div className="board">
+      <canvas
+        ref={canvasRef}
+        id="board"
+        width="200"
+        height="100"
+        style={boardStyle}
+        onClick={handleCanvasClick}
+      ></canvas>
+    </div>
+  );
 }
 export default SoccerBoard;
