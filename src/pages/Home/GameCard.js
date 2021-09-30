8d93c1ce861aa797f09312c2398c017c51314bd6
agakesik <aga.k396@gmail.com>
2021-09-30 14:07:35 +0200

    Add popup to index page with game opponent choice

--- src/pages/Home/GameCard.js
+++ src/pages/Home/GameCard.js
@@ -1,14 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import Popup from "./Popup";
 import "./style.scss";
 
 function GameCard(props) {
+  const [isPopup, setIsPopup] = useState(false);
+
+  const togglePopup = () => {
+    setIsPopup(!isPopup);
+    console.log("hello");
+  };
+
+  const popupContent = (
+    <>
+      <h3>{props.title}</h3>
+      <p>choose how you want to play</p>
+      <a className="card-link-button" href={props.route}>
+        with human
+      </a>
+      <a
+        className="card-link-button"
+        href={props.routeAI ? props.routeAI : "/"}
+      >
+        with ai
+      </a>
+    </>
+  );
+
   return (
     <div className="card-wrapper">
-      <a href={props.route}>
+      <Popup
+        isPopup={isPopup}
+        togglePopup={() => togglePopup()}
+        content={popupContent}
+      />
+
+      {/* TODO: change to button and fix styling */}
+      <div onClick={togglePopup}>
         <div className="card-button">
           <span className="card-title">{props.title}</span>
         </div>
-      </a>
+      </div>
       <span className="card-player-info">
         currently playing: {props.players} <strong>totally real</strong> people!
       </span>
